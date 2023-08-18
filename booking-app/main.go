eb83476f721ce3d4ceda335ad5db89763b30d570
Victor Wilberforce <victorwilberforce2310@gmail.com>
2023-08-17 22:34:10 -0400

    Learning about returns functions and user input in go

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,14 +4,28 @@ import "fmt"
 
 
 func welcomeMsg(conferenceName string, conferenceTickets int, remainingTickets int){
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and ", remainingTickets, "are still available.")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
 }
 
+func userInput()string{
+	var userName string
+	var userTickets int
+	fmt.Print("Enter user name\n")
+	fmt.Scan(&userName)
+	fmt.Print("Enter number of tickets\n")
+	fmt.Scan(&userTickets)
+
+	message := fmt.Sprintf("User %v booked %v tickets", userName, userTickets)
+	return message
+}
+
 func main(){
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
 	welcomeMsg(conferenceName, conferenceTickets, remainingTickets)
+	result:=userInput()
+	fmt.Println(result)	
 }
