165b93107dfd6b74ac7269a0b8be56ad1f9e9bb0
Victor Wilberforce <victorwilberforce2310@gmail.com>
2023-08-20 18:42:11 -0400

    Refactor user input and booking ticket

--- booking-app/main.go
+++ booking-app/main.go
@@ -16,33 +16,34 @@ func welcomeMsg(conferenceName string, conferenceTickets int, remainingTickets u
 	fmt.Println()
 }
 
-func getUserInput(conferenceName string, remainingTickets uint) {
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
-	for {
-		fmt.Println("Enter your first name")
-		fmt.Scan(&firstName)
-		
-		fmt.Println("Enter your last name")
-		fmt.Scan(&lastName)
-		
-		fmt.Println("Enter your email name")
-		fmt.Scan(&email)
-		
-		fmt.Println("Enter number of tickets")
-		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+	
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+	
+	fmt.Println("Enter your email name")
+	fmt.Scan(&email)
+	
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+	
+	return firstName, lastName, email, userTickets
+}
 
-		confirmationMessage := fmt.Sprintf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		ticketCheck := fmt.Sprintf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-		
-		fmt.Print(confirmationMessage)
-		fmt.Print(ticketCheck)
-		fmt.Print(bookingList(firstName, lastName))
-	}
+func bookingTickets(firstName string, lastName string, email string, userTickets uint){
+	remainingTickets = remainingTickets - userTickets
+
+	confirmationMessage := fmt.Sprintf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Print(confirmationMessage)
+	ticketCheck := fmt.Sprintf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	print(ticketCheck)
 }
 
 func bookingList(firstName string, lastName string) string {
@@ -62,5 +63,5 @@ func bookingList(firstName string, lastName string) string {
 
 func main(){
 	welcomeMsg(conferenceName, conferenceTickets, remainingTickets)
-	getUserInput(conferenceName, remainingTickets)
+	getUserInput()
 }
